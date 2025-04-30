04a9265df475b37b3647701c300d27117d603b2b
Caleb Odobi Chibueze <107697220+Chibez@users.noreply.github.com>
2025-04-30 11:04:48 +0100

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
 
               <h3 className="contact-card-title">Gmail</h3>
               <span className="contact-card-data">
-                odobicaleb@gmail.com
+                IfeanyiEzeugwu@gmail.com
               </span>
 
               <a
@@ -65,7 +65,7 @@ const Contact = () => {
               <i className="bx bxl-twitter contact-card-icon"></i>
 
               <h3 className="contact-card-title">X fomaerly Twitter</h3>
-              <span className="contact-card-data">odobicaleb</span>
+              <span className="contact-card-data">IfeanyiEzeugwu</span>
 
               <a
                 href="https://chatgpt.com/"
